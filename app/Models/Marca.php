b3680326a624b30eeb5ba1c4cec4fd0398ebd158
Carlos Eduardo Vieira <carlosportalsocial@gmail.com>
2023-01-14 20:47:28 -0300

    created rules method in model Marca

--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -8,5 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Marca extends Model
 {
     use HasFactory;
+
     protected $fillable = ['nome', 'imagem'];
+
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required|unique:marcas|min:3',
+            'imagem' => 'required'
+        ];
+    }
+
+    public function feedback(): array
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => 'O nome da marca já existe',
+            'nome.min' => 'O nome deve ter no minímo 3 caracteres'
+        ];
+    }
 }
