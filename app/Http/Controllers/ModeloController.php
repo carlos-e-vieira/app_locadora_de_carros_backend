bae68ff9ae9711e6f0d8d36b2259e9f9022d0b87
Carlos Eduardo Vieira <carlosportalsocial@gmail.com>
2023-01-17 18:42:35 -0300

    criado all métodos para o controlador modelo

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -4,61 +4,109 @@ namespace App\Http\Controllers;
 
 use App\Models\Modelo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ModeloController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    private Modelo $modelo;
+
+    public function __construct(Modelo $modelo)
+    {
+        $this->modelo = $modelo;
+    }
+
     public function index()
     {
-        //
+        return response()->json($this->modelo->all(), 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->modelo->rules(), $this->modelo->feedback());
+
+        $image = $request->file('imagem');
+        $imageEndpoint = $image->store('imagens/modelos', 'public');
+
+        $modelo = $this->modelo->create([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imageEndpoint,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs
+        ]);
+
+        return response()->json($modelo, 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Modelo  $modelo
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Modelo $modelo)
+    public function show($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if ($modelo === null) {
+            return response()->json(['success' => false], 404);
+        }
+
+        return response()->json($modelo, 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Modelo  $modelo
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, $id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if ($modelo === null) {
+            return response()->json(['success' => false], 404);
+        }
+
+        if ($request->method() === 'PATCH') {
+            $dinamicsRules = array();
+
+            foreach ($modelo->rules() as $input => $rule) {
+
+                if (array_key_exists($input, $request->all())) {
+                    $dinamicsRules[$input] = $rule;
+                }
+
+            }
+            $request->validate($dinamicsRules, $this->modelo->feedback());
+        }
+
+        if ($request->method() === 'PUT') {
+            $request->validate($this->modelo->rules(), $this->modelo->feedback());
+        }
+
+        if ($request->file('imagem')) {
+            Storage::disk('public')->delete($modelo->imagem);
+        }
+        
+        $image = $request->file('imagem');
+        $imageEndpoint = $image->store('imagens/modelos', 'public');
+
+        $modelo->update([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imageEndpoint,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs
+        ]);
+
+        return response()->json($modelo, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Modelo  $modelo
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Modelo $modelo)
+    public function destroy($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if ($modelo === null) {
+            return response()->json(['success' => false], 404);
+        }
+
+        Storage::disk('public')->delete($modelo->imagem);
+
+        $modelo->delete();
+        return response()->json(['success' => true], 200);
     }
 }
--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -4,8 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rule;
 
 class Modelo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'marca_id', 'nome', 'imagem', 'numero_portas', 'lugares', 'air_bag', 'abs'
+    ];
+
+    public function rules(): array
+    {
+        return [
+            'marca_id' => 'exists:marcas,id', // verifica se o id existe na table marcas
+            'nome' => 'required|', Rule::unique('modelos')->ignore($this->id, 'id'). '|min:3',
+            'imagem' => 'required|file|mimes:png,jpeg,jpg',
+            'numero_portas' => 'required|integer|digits_between:1,5', // aceita numeros de 1 a 5
+            'lugares' => 'required|integer|digits_between:1,20',
+            'air_bag' => 'required|boolean', // a validação aceita true ou false, 0 e 1, "0" e "1"
+            'abs' => 'required|boolean'
+        ];
+    }
+
+    public function feedback(): array
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+
+            'nome.unique' => 'O nome da marca já existe',
+            'nome.min' => 'O nome deve ter no minímo 3 caracteres',
+
+            'imagem.mimes' => 'O arquivo deve ser uma imagem PNG, JPEG ou JPG',
+
+            'numero_portas.digits_between' => 'O número de portas deve ser entre 1 e 5',
+            'lugares.digits_between' => 'O número de lugares deve ser entre 1 e 20',
+        ];
+    }
 }
