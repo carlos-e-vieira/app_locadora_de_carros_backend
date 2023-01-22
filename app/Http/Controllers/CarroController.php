5af9610add3eab2a33ae4d6243ad63ee9a89fe6c
Carlos Eduardo Vieira <carlosportalsocial@gmail.com>
2023-01-22 09:40:27 -0300

    api rest para o Resource Carro

--- app/Http/Controllers/CarroController.php
+++ app/Http/Controllers/CarroController.php
@@ -3,63 +3,112 @@
 namespace App\Http\Controllers;
 
 use App\Models\Carro;
-use App\Http\Requests\StoreCarroRequest;
-use App\Http\Requests\UpdateCarroRequest;
+use App\Repositories\CarroRepository;
+use Illuminate\Http\Request;
 
 class CarroController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    private Carro $carro;
+
+    public function __construct(Carro $carro)
     {
-        //
+        $this->carro = $carro;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreCarroRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreCarroRequest $request)
+    public function index(Request $request)
     {
-        //
+        $carroRepository = new CarroRepository($this->carro);
+
+        // condição de busca por atributos do modelo
+        if ($request->has('atributos_modelo')) {
+            $atributos_modelo = 'modelo:id,'. $request->atributos_modelo;
+            $carroRepository->selectAtributosRegistrosRelacionados($atributos_modelo);
+        
+        } else {
+            $carroRepository->selectAtributosRegistrosRelacionados('modelo');
+        }
+
+        // condição de busca com filtro
+        if ($request->has('filtro')) {
+            $carroRepository->filtro($request->filtro);
+        }
+
+        // condição de busca por atributos do marca
+        if ($request->has('atributos')) {
+            $carroRepository->selectAtributos($request->atributos);
+        }
+
+        return response()->json($carroRepository->getResultado(), 200);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Carro  $carro
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Carro $carro)
+    public function store(Request $request)
     {
-        //
+        $request->validate($this->carro->rules(), $this->carro->feedback());
+        $carro = $this->carro->create(
+            [
+                'modelo_id' => $request->modelo_id,
+                'placa' => $request->placa,
+                'disponivel' => $request->disponivel,
+                'km' => $request->km
+            ]
+        );
+
+        return response()->json($carro, 201);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateCarroRequest  $request
-     * @param  \App\Models\Carro  $carro
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateCarroRequest $request, Carro $carro)
+    public function show($id)
     {
-        //
+        // adicionando o relacionamento - uma marca tem muito modelos
+        $carro = $this->carro->with('modelo')->find($id);
+
+        if ($carro === null) {
+            return response()->json(['success' => false], 404);
+        }
+
+        return response()->json($carro, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Carro  $carro
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Carro $carro)
+    public function update(Request $request, $id)
     {
-        //
+        $carro = $this->carro->find($id);
+
+        if ($carro === null) {
+            return response()->json(['success' => false], 404);
+        }
+
+        if ($request->method() === 'PATCH') {
+            $dinamicsRules = array();
+
+            foreach ($carro->rules() as $input => $rule) {
+
+                if (array_key_exists($input, $request->all())) {
+                    $dinamicsRules[$input] = $rule;
+                }
+
+            }
+            $request->validate($dinamicsRules, $this->carro->feedback());
+        }
+
+        if ($request->method() === 'PUT') {
+            $request->validate($this->carro->rules(), $this->carro->feedback());
+        }
+
+        // Preencher objeto carro com os dados da request
+        $carro->fill($request->all());
+        $carro->save();
+
+        return response()->json($carro, 200);
+    }
+
+    public function destroy($id)
+    {
+        $carro = $this->carro->find($id);
+
+        if ($carro === null) {
+            return response()->json(['success' => false], 404);
+        }
+
+        $carro->delete();
+        return response()->json(['success' => true], 200);
     }
 }
--- app/Models/Carro.php
+++ app/Models/Carro.php
@@ -4,8 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rule;
 
 class Carro extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'modelo_id', 'placa', 'disponivel', 'km'
+    ];
+
+    public function rules()
+    {
+        return [
+            'modelo_id' => 'required|exists:modelos,id',
+            //'placa' => 'required|unique|min:7|max:7',
+            'placa' => 'required|', Rule::unique('carros')->ignore($this->id, 'id'), 
+            'disponivel' => 'required|boolean',
+            'km' => 'required|integer'
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório.',
+
+            'placa.unique' => 'Essa placa já existe',
+            'placa.min' => 'A placa deve ter 7 caracteres',
+            'placa.max' => 'A placa deve ter 7 caracteres',
+        ];
+    }
+
+    public function modelo()
+    {
+        // um carro pertence a um modelo
+        return $this->belongsTo(Modelo::class);
+    }
 }
